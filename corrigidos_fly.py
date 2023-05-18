b699dbb94bcf707773e71aabbcfe3844a1e76bb4
gabriel-quintiliano <115936067+gabriel-quintiliano@users.noreply.github.com>
2023-05-18 00:34:17 -0300

    Add files via upload

---
+++ corrigidos_fly.py
@@ -0,0 +1,40 @@
+# import auto_fly_stable as af
+import auto_fly as af
+# import time
+
+def main():
+    
+    data = af.gerar_data_referencia()
+    options = af.user_options
+    
+    """if options['verificar_execucao_anterior'].lower() == 's':
+        print('verificou')
+        af.verificar_se_ja_foi_emitido()"""
+
+    af.abrir_e_logar(options['user'], options['password'], options['organograma'])
+    
+    af.gerar_rel_processos(data)
+    af.abrir_rel_e_extrair_processos(corrigidos=True)
+
+    if options['emitir_etiquetas_corrigidas'].lower() == 's':
+        af.emitir_etiquetas()
+
+    if options['emitir_planilhas_corrigidas'].lower() == 's':
+        af.emitir_comprovantes_de_confirmação_isolados()
+
+    if options['emitir_protocolos_corrigidos'].lower() == 's':
+        af.emitir_comprovantes_de_abertura_isolados()
+    
+    af.demonstrar_processos_sem_andamento()
+    af.demonstrar_processos_com_andamento_incomum()
+
+    if options['abrir_relatorios_corrigidos'].lower() == 's':
+        af.abrir_relatorios_emitidos()
+    else:
+        af.acessar_no_menu('Relatórios', 'Gerenciador de relatórios')
+    
+    af.esperar_usuario_e_sair()
+
+
+if __name__ == '__main__':
+    main()
