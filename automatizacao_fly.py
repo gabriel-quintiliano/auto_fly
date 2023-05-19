a2cb7574feb2189532827b963c5d3e6a292dc20e
gabriel-quintiliano <115936067+gabriel-quintiliano@users.noreply.github.com>
2023-05-18 22:11:35 -0300

    Add files via upload

--- automatizacao_fly.py
+++ automatizacao_fly.py
@@ -10,31 +10,30 @@ def main():
     data = af.gerar_data_referencia()
     options = af.user_options
     
-    """if options['verificar_execucao_anterior'].lower() == 's':
-        print('verificou')
-        af.verificar_se_ja_foi_emitido()"""
+    if options['verificar_execucao_anterior'].lower() == 's':
+        af.verificar_se_ja_foi_emitido()
 
     af.abrir_e_logar(options['user'], options['password'], options['organograma'])
     
     af.gerar_rel_processos(data)
-    af.abrir_rel_e_extrair_processos(corrigidos=True)
+    af.abrir_rel_e_extrair_processos()
 
-    if options['emitir_etiquetas_corrigidas'].lower() == 's':
+    if options['emitir_etiquetas'].lower() == 's':
         af.emitir_etiquetas()
 
-    if options['emitir_planilhas_corrigidas'].lower() == 's':
-        af.emitir_comprovantes_de_confirmação_isolados()
-
-    if options['emitir_protocolos_corrigidos'].lower() == 's':
-        af.emitir_comprovantes_de_abertura_isolados()
+    if options['emitir_planilhas'].lower() == 's':
+        af.emitir_comprovantes_de_confirmação(data)
     
     af.demonstrar_processos_sem_andamento()
     af.demonstrar_processos_com_andamento_incomum()
 
-    """if options['abrir_relatorios_corrigidos'].lower() == 's':
+    if options['abrir_relatorios'].lower() == 's':
         af.abrir_relatorios_emitidos()
-    else:"""
-    af.acessar_no_menu('Relatórios', 'Gerenciador de relatórios')
+    else:
+        af.acessar_no_menu('Relatórios', 'Gerenciador de relatórios')
+    
+    if options['armazenar_info_execucao'].lower() == 's':
+        af.register_excecution_info()
     
     af.esperar_usuario_e_sair()
 
